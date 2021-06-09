725bf9cc4d5fac06a26755e8506be619fa789321
Shehab Solyman <Shehabamgad134@gmail.com>
2021-06-09 18:44:14 +0200

    replaced switch case with more generic if statement

--- src/Architecture.java
+++ src/Architecture.java
@@ -11,88 +11,25 @@ public class Architecture {
     }
 
     public void writeRegister (String register, int value) throws ArchitectureExceptions {
-
         if(register.equals("R0"))
             throw new ArchitectureExceptions("The zero register cannot be overwritten");
 
-        switch(register){
-            case "R1" :  registers[1] = value; break;
-            case "R2" :  registers[2] = value; break;
-            case "R3" :  registers[3] = value; break;
-            case "R4" :  registers[4] = value; break;
-            case "R5" :  registers[5] = value; break;
-            case "R6" :  registers[6] = value; break;
-            case "R7" :  registers[7] = value; break;
-            case "R8" :  registers[8] = value; break;
-            case "R9" :  registers[9] = value; break;
-            case "R10" : registers[10] = value; break;
-            case "R11" : registers[11] = value; break;
-            case "R12" : registers[12] = value; break;
-            case "R13" : registers[13] = value; break;
-            case "R14" : registers[14] = value; break;
-            case "R15" : registers[15] = value; break;
-            case "R16" : registers[16] = value; break;
-            case "R17" : registers[17] = value; break;
-            case "R18" : registers[18] = value; break;
-            case "R19" : registers[19] = value; break;
-            case "R20" : registers[20] = value; break;
-            case "R21" : registers[21] = value; break;
-            case "R22" : registers[22] = value; break;
-            case "R23" : registers[23] = value; break;
-            case "R24" : registers[24] = value; break;
-            case "R25" : registers[25] = value; break;
-            case "R26" : registers[26] = value; break;
-            case "R27" : registers[27] = value; break;
-            case "R28" : registers[28] = value; break;
-            case "R29" : registers[29] = value; break;
-            case "R30" : registers[30] = value; break;
-            case "R31" : registers[31] = value; break;
-            case "PC"  : registers[32] = value; break;
-            default:
+        if(register.equals("PC")){
+            registers[32] = value;
         }
-
+        else if(register.charAt(0)=='R' && Integer.parseInt(register.substring(1))>0 && Integer.parseInt(register.substring(1))<32){
+            registers[Integer.parseInt(register.substring(1))] = value;
+        }
     }
 
     public int readRegister (String register) throws ArchitectureExceptions {
-
-        switch(register){
-            case "R0" :  return registers[0] ;
-            case "R1" :  return registers[1] ;
-            case "R2" :  return registers[2] ;
-            case "R3" :  return registers[3] ;
-            case "R4" :  return registers[4] ;
-            case "R5" :  return registers[5] ;
-            case "R6" :  return registers[6] ;
-            case "R7" :  return registers[7] ;
-            case "R8" :  return registers[8] ;
-            case "R9" :  return registers[9] ;
-            case "R10" : return registers[10] ;
-            case "R11" : return registers[11] ;
-            case "R12" : return registers[12] ;
-            case "R13" : return registers[13] ;
-            case "R14" : return registers[14] ;
-            case "R15" : return registers[15] ;
-            case "R16" : return registers[16] ;
-            case "R17" : return registers[17] ;
-            case "R18" : return registers[18] ;
-            case "R19" : return registers[19] ;
-            case "R20" : return registers[20] ;
-            case "R21" : return registers[21] ;
-            case "R22" : return registers[22] ;
-            case "R23" : return registers[23] ;
-            case "R24" : return registers[24] ;
-            case "R25" : return registers[25] ;
-            case "R26" : return registers[26] ;
-            case "R27" : return registers[27] ;
-            case "R28" : return registers[28] ;
-            case "R29" : return registers[29] ;
-            case "R30" : return registers[30] ;
-            case "R31" : return registers[31] ;
-            case "PC"  : return registers[32] ;
-            default:
+        if(register.equals("PC")){
+            return registers[32];
+        }
+        else if(register.charAt(0)=='R' && Integer.parseInt(register.substring(1))>=0 && Integer.parseInt(register.substring(1))<32){
+            return registers[Integer.parseInt(register.substring(1))];
         }
         return 0;
-
     }
 
     public void writeMem (int word, int value, boolean instruction) throws ArchitectureExceptions {
